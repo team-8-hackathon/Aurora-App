ac0f746711601221ae8b5039da927a01292e0bd6
Emily <mocemmy@aol.com>
2023-10-24 12:39:28 -0400

    added db models

---
+++ app/models/testimonial.py
@@ -0,0 +1,36 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.orm import validates
+from datetime import datetime
+
+class Testimonial(db.Model):
+    __tablename__ = 'testimonials'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    #add a default url to profile pics later
+    profile_pic = db.Column(db.String(1000))
+    name = db.Column(db.String(200), nullable=False)
+    stars = db.Column(db.Integer, nullable=False)
+    body = db.Column(db.String(1000), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    #validate stars is 1 - 5
+    @validates('stars')
+    def validate_stars(self, key, value):
+        if not 1 <= value <= 5:
+            raise ValueError(f'Invalid stars {value}')
+        return value
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'profile_pic': self.profile_pic,
+            'name': self.name,
+            'stars': self.stars,
+            'body': self.body,
+            'created_at': self.created_at,
+        }
---
+++ app/models/topic.py
@@ -0,0 +1,24 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .blog_topics import blog_topics
+from datetime import datetime
+
+class Topic(db.Model):
+    __tablename__ = 'topics'
+
+    if environment == 'production':
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    topic = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now)
+
+    #define relationships through the join table blog_topics
+    blogs = db.relationship('Blog', secondary=blog_topics, back_populates='topics')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'topic': self.topic
+        }
+
