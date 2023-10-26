f42b75cc7d770611b32a4776b8efb42638a814bb
Emily <mocemmy@aol.com>
2023-10-26 10:31:33 -0400

    added admin protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { authenticate } from "./store/session";
+import BlogForm from "./components/BlogComponents/BlogForm";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 
 
@@ -19,6 +21,11 @@ function App() {
           <Route exact path='/'>
             <h1>React Home Page</h1>
           </Route>
+          <ProtectedRoute>
+            <Route path='/admin'>
+              <h1>Admin Page</h1>
+            </Route>
+          </ProtectedRoute>
         </Switch>
       )}
     </>
---
+++ frontend/src/components/auth/LoginModal.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import { useModal } from "../../context/Modal";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/session";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const LoginModal = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState();
+    const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login(username, password));
+        if (data) {
+            setErrors(data);
+        } else {
+            closeModal()
+        }
+    };
+
+    const handleCancel = () => {
+        history.push('/') //If cancel login, redirect to home page
+    }
+
+    return (
+        <>
+            <h1>Admin Login</h1>
+            <form>
+                <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)}/>
+                <input type='password' placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <button type='submit' onClick={handleSubmit}>Login</button>
+                <button onClick={handleCancel}>Cancel</button>
+            </form>
+            {errors && <p className="errors">{errors}</p>}
+        </>
+    );
+}
+
+export default LoginModal;
