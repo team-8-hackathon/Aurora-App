95a381a175587033544eb8e74383a343e7a08324
Emily <mocemmy@aol.com>
2023-10-27 19:46:45 -0400

    styled blog thumbnails

--- frontend/src/components/BlogComponents/BlogThumbnail.js
+++ frontend/src/components/BlogComponents/BlogThumbnail.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 const BlogThumbnail = ({ topic, blog }) => {
+    const colors = { 1: '#ffe27a', 2: '#ec9fb8', 3: '#9acfb1', 4: '#f4a182'}
+    let color;
+    if(1 <= topic.id <= 4){
+        color=colors[topic.id]
+    } else color = colors[0]
     return (
-        <div
+        <div className="blog-thumbnail"
             style={{
                 backgroundImage:`url(${blog.thumbnail})`,
                 backgroundSize: "cover"
             }}
         >
-            <p>{topic.topic}</p>
-            <p>{blog.title}</p>
+            <p className="topic-tag" style={{'backgroundColor': `${color}`}}>{topic.topic}</p>
+            <p className="title-tag">{blog.title}</p>
         </div>
     )
 }
--- frontend/src/components/BlogComponents/BrowseBlogs.js
+++ frontend/src/components/BlogComponents/BrowseBlogs.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { thunkGetSingleTopic } from '../../store/topics';
 import BlogThumbnail from './BlogThumbnail';
+import './BrowseBlogs.css'
 
 
 const BrowseBlogs = () => {
@@ -15,25 +16,28 @@ const BrowseBlogs = () => {
         dispatch(thunkGetSingleTopic(topicId))
     }, [dispatch, topicId])
 
-    
-    if(!topic) return null;
+
+    if (!topic) return null;
     const blogs = topic.blogs;
 
     const showMoreArticles = () => {
-        if(numArticles < blogs.length){
+        if (numArticles < blogs.length) {
             setNumArticles(numArticles + 6)
         }
         console.log(blogs.slice(0, numArticles))
     }
-    
+
     return (
         <>
             <h2>{topic.topic}</h2>
-            {blogs.slice(0,numArticles).map(blog => (
-                <Link key={blog.id} to={`/blogs/${blog.id}`}>
-                    <BlogThumbnail topic={topic} blog={blog} />
-                </Link>
-            ))}
+            <div className='blog-container'>
+
+                {blogs.slice(0, numArticles).map(blog => (
+                    <Link id='blog-thumbnail-link' key={blog.id} to={`/blogs/${blog.id}`}>
+                        <BlogThumbnail topic={topic} blog={blog} />
+                    </Link>
+                ))}
+            </div>
             {numArticles < blogs.length && <button onClick={showMoreArticles}>Show more articles</button>}
         </>
     )
