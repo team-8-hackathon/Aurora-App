f51b44f9fd1684078de1fca7435870fe36a03691
Emily <mocemmy@aol.com>
2023-10-28 19:44:43 -0400

    styled login form

--- frontend/src/components/auth/LoginModal.js
+++ frontend/src/components/auth/LoginModal.js
@@ -4,6 +4,8 @@ import { useModal } from "../../context/Modal";
 import { useDispatch } from "react-redux";
 import { login } from "../../store/session";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import './LoginModal.css'
+
 
 const LoginModal = () => {
     const [username, setUsername] = useState('');
@@ -17,24 +19,27 @@ const LoginModal = () => {
         e.preventDefault();
         const data = await dispatch(login(username, password));
         if (data) {
+            console.log(data)
             setErrors(data);
         } else {
-            closeModal()
+            history.push('/admin')
         }
     };
 
     const handleCancel = () => {
-        history.push('/') //If cancel login, redirect to home page
+        history.push('/') //If cancel login, redirect to  home page
     }
 
     return (
-        <div>
-            <h1>Admin Login</h1>
-            <form>
-                <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)}/>
-                <input type='password' placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
-                <button type='submit' onClick={handleSubmit}>Login</button>
-                <button onClick={handleCancel}>Cancel</button>
+        <div className="login-form-container">
+            <h1 className="login-title">Admin Login</h1>
+            <form className="login-form">
+                <input className="login-input" type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
+                <input className="login-input" type='password' placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <div className="button-container">
+                    <button className='blog-post-button cancel' type='button' onClick={handleCancel}>Cancel</button>
+                    <button className="blog-post-button submit" type='submit' onClick={handleSubmit}>Login</button>
+                </div>
             </form>
             {errors && <p className="errors">{errors}</p>}
         </div>
