85e6ea72b55a999ccaae58c6f658d14086079da3
gabriel <gabeh.reiter@gmail.com>
2023-10-30 09:37:46 -0400

    testimonial updates

--- app/api/testimonial_routes.py
+++ app/api/testimonial_routes.py
@@ -7,8 +7,11 @@ from app.api.auth_routes import validation_errors_to_error_messages
 testimonial_routes = Blueprint('testimonial', __name__)
 
 @testimonial_routes.route('/')
-def hello():
-    return "Testimonial route"
+def get_testimonials():
+     testimonials = Testimonial.query.order_by(Testimonial.stars.desc()).all()
+     testimonials = testimonials[0:4]
+     return {'testimonials': [testimonial.to_dict() for testimonial in testimonials]}, 201
+
 
 
 @testimonial_routes.route('/new', methods=["POST"])
@@ -20,16 +23,17 @@ def create_testimonial():
   form['csrf_token'].data = request.cookies['csrf_token']
   if form.validate_on_submit():
     name = form.data['name']
-    profile_pic = form.data['profile_pic']
+    if form.data['profile_pic']:
+      profile_pic = form.data['profile_pic']
     stars = form.data['stars']
     body = form.data['body']
 
     testimonial = Testimonial(name=name, stars=stars, body=body, profile_pic=profile_pic)
     db.session.add(testimonial)
     db.session.commit()
-    print('Testimonial Saved')
+    print('----------Testimonial Saved')
     testimonials = Testimonial.query.order_by(Testimonial.stars.desc()).all()
     return {'testimonials': [testimonial.to_dict() for testimonial in testimonials]}, 201
     # return testimonial.to_dict(), 201
 
-  return {"ErRors ": validation_errors_to_error_messages(form.errors)}, 401
+  return {"ERRORS ": validation_errors_to_error_messages(form.errors)}, 401
