5f1df814865300851ee7be71f6799a69ed377c2d
Freddy Rendon <frendonjr03@gmail.com>
2023-11-01 20:29:52 -0400

    add script to local directory

---
+++ migrate_and_seed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Activate your Python virtual environment
+pipenv shell
+
+# Upgrade the database to the latest version
+flask db upgrade
+
+# Seed the database
+flask seed undo
+flask seed all
