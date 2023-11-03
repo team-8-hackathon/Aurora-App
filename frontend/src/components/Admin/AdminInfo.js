1e179791701747ac579cb961e459031d46be051c
Emily <mocemmy@aol.com>
2023-11-03 09:38:10 -0400

    finished styling admin info page

--- frontend/src/components/Admin/AdminInfo.js
+++ frontend/src/components/Admin/AdminInfo.js
@@ -2,14 +2,15 @@ import { Link } from "@mui/material";
 import { useSelector } from "react-redux";
 import OpenModalButton from "../UtilityComponents/OpenModalButton";
 import EditAdmin from './EditAdmin'
+import './AdminInfo.css'
 
 const AdminInfo = () => {
     const admin = useSelector(state => state.session.user)
     return (
         <div className="login-form-container">
-            <h2>Admin Info</h2>
-            <p>Username: {admin.username}</p>
-            <p>Password last changed: {admin.updated_at}</p>
+            <h2 className="admin-info-title">Admin Info</h2>
+            <p className="admin-info">Username: {admin.username}</p>
+            <p className="admin-info">Password last changed: {admin.updated_at}</p>
             <OpenModalButton 
             modalComponent={<EditAdmin />} className="blog-post-button submit" buttonText='Update Password'/>
         </div>
