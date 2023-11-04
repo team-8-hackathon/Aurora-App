69f51f2736914b666b6efd870d37a30b58e0eb58
Emily <mocemmy@aol.com>
2023-11-03 20:06:53 -0400

    added splash blueprint

---
+++ app/api/splash_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from app.forms import SplashParagraphForm
+from app.models import SplashParagraph, db
+
+splash_routes = Blueprint('splash', __name__)
+
+#Get all splash paragraphs
+@splash_routes.route('/')
+def get_all_splash():
+    """
+    Get all splash paragraphs
+    """
+    splashes = SplashParagraph.query.all()
+
+    return {'paragraphs': [splash.to_dict() for splash in splashes]}
+
