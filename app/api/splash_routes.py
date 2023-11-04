55a06680f4014bf5b1058f47e48f63c8e040e1d9
Emily <mocemmy@aol.com>
2023-11-03 20:34:45 -0400

    added tests to splash

--- app/api/splash_routes.py
+++ app/api/splash_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from flask_login import login_required, current_user
 from app.forms import SplashParagraphForm
 from app.models import SplashParagraph, db
+from app.api.auth_routes import validation_errors_to_error_messages
 
 splash_routes = Blueprint('splash', __name__)
 
@@ -15,3 +16,35 @@ def get_all_splash():
 
     return {'paragraphs': [splash.to_dict() for splash in splashes]}
 
+#edit a splash paragraph by its id
+@splash_routes.route('/<int:id>/edit', methods=["PATCH"])
+@login_required
+def edit_splash(id):
+    """
+    Edit a splash paragraph's header and paragraph by id
+    """
+
+    splash = SplashParagraph.query.get(id)
+    if not splash:
+        return {"errors", "splash paragraph not found"}, 404
+    form = SplashParagraphForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if form.data['header']:
+            splash.header = form.data['header']
+        if form.data['paragraph']:
+            splash.paragraph = form.data['paragraph']
+        db.session.commit()
+        return splash.to_dict()
+    return {"errors", validation_errors_to_error_messages(form.errors)}
+
+#get a splash section by id
+splash_routes.route('/<int:id>')
+def get_single_splash(id):
+    """
+    Query to get a splash paragraph entry by its id
+    """
+    splash = SplashParagraph.query.get(id)
+    if not splash:
+        return {"errors", "splash paragraph not found"}, 404
+    return splash.to_dict()
--- app/forms/splashparagraph_form.py
+++ app/forms/splashparagraph_form.py
@@ -3,6 +3,5 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class SplashParagraphForm(FlaskForm):
-    paragraph = StringField('paragraph', validators=[DataRequired()])
-    title = StringField('title', validators=[DataRequired()])
-    header = StringField('header', validators=[DataRequired()])
+    paragraph = StringField('paragraph')
+    header = StringField('header')
