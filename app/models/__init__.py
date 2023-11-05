c72097c06a7581805a7c28d6376a1af241e68997
Emily <mocemmy@aol.com>
2023-11-05 18:10:08 -0500

    removed subscribers table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,6 @@
 from .db import db, environment, SCHEMA
 from .admin import Admin
 from .blog import Blog
-from .subscriber import Subscriber
 from .testimonial import Testimonial
 from .splash_paragraphs import SplashParagraph
 from .topic import Topic
